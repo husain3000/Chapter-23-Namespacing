e35d8c0deef0cda50536a21f62da4af1c423188e
husain3000 <73273019+husain3000@users.noreply.github.com>
2023-02-27 11:40:31 +0530

    first commit

---
+++ namespacing.js
@@ -0,0 +1,15 @@
+var boy = {
+boyName: function() {
+console. log( "The boy name is John." )
+}
+};
+var girl = {
+girlName: function() {
+console. log( "The girl name is Kate. ")
+}
+};
+boy. boyName ()
+girl. girlName()
+boy. boyName ()
+girl. girlName()
+//namespacing in nothing but a container that is used to store classes ,function as well as variable which can be repeated multiple time in our code without causing any error//
